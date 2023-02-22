3d719cee9ad11b97425a1021bcb37219c4ca738e
Atul <atulindap91@gmail.com>
2023-02-22 13:12:45 +0530

    made read_image and blur_outline async

--- Image_Read_Write/Read_Write_Image.py
+++ Image_Read_Write/Read_Write_Image.py
@@ -1,5 +1,4 @@
 import pydicom as dicom
-import asyncio
 import os
 import numpy as np
 from PIL import Image
@@ -9,9 +8,87 @@ current_directory = os.getcwd()
 
 unprocess_images_path = current_directory + str("/Unprocess_images/")
 intermediate_image_process_path = current_directory + str("/Intermediate_process_images/")
+final_image_path = current_directory + str('/Final_processed_images/')
 
+# def blockMerge(array, imgWidth, imgHeight):
+#     """Merges image blocks into a grayscale image.
+#
+#     Args:
+#         array (ndarray): Containing image blocks with third axis representing the number of blocks.
+#         imgWidth (int): Width of full image.
+#         imgHeight (int): Height of full image.
+#
+#     Returns:
+#         (ndarray): Array containing full grayscale image.
+#     """
+#
+#     # Dimension of the input image
+#     blockSizeX = array.shape[1]
+#     blockSizeY = array.shape[0]
+#
+#     # Number of blocks along an axis
+#     blocksAlongX = imgWidth / blockSizeX
+#     blocksAlongY = imgHeight / blockSizeY
+#
+#     # Creating Numpy Array for output image
+#     outputImage = np.zeros((imgHeight, imgWidth))
+#
+#     # Counter for indexing each block from input array
+#     depthCounter = 0
+#
+#     # Starting position of X
+#     x = 0
+#
+#     # Starting position of Y
+#     y = 0
+#
+#     while y < imgHeight:
+#         while x < imgWidth:
+#             try:
+#                 outputImage[y: (y + blockSizeY),
+#                 x: (x + blockSizeX)] = array[:, :, depthCounter]
+#                 x += blockSizeX
+#                 depthCounter += 1
+#             except:
+#                 pass
+#         y += blockSizeY
+#         x = 0
+#
+#     return outputImage
+#
+#
+# def saveBlock(array):
+#     """Saves blocks or full image as JPG.
+#
+#     Args:
+#         array (ndarray): Contains either blocks or full image.
+#     """
+#
+#     # Blocks
+#     if len(array.shape) == 3:
+#
+#         # Creating folder for blocks
+#         dst_folder = intermediate_image_process_path
+#
+#         # Saving blocks
+#         for i in range(array.shape[2]):
+#             imgObject = Image.fromarray(array[:, :, i].astype('uint8'), 'L')
+#             imgName = f"{str(dst_folder)}/block_{i}.jpg"
+#             imgObject.save(imgName)
+#
+#     # Single image
+#     elif len(array.shape) == 2:
+#
+#         # Creating folder for blocks
+#         dst_folder = final_image_path
+#
+#
+#         # Saving image
+#         imgObject = Image.fromarray(array[:, :].astype('uint8'), 'L')
+#         imgName = f"{str(dst_folder)}/imageFull.jpg"
+#         imgObject.save(imgName)
 
-def read_image(unprocess_images_path):
+async def read_image(unprocess_images_path):
 
     dir_list = os.listdir(unprocess_images_path)
 
@@ -29,27 +106,25 @@ def read_image(unprocess_images_path):
 
         name, ext = os.path.splitext(filename)
         img = Image.open(os.path.join(intermediate_image_process_path, filename))
-        w, h = 250, 320
-        d = 25
+        w, h = img.size
+        d = 250
 
         newfolder = 'image_'+str(i)
 
         isExist = os.path.exists(intermediate_image_process_path+'/'+newfolder)
+
         if not isExist:
             os.makedirs(intermediate_image_process_path+'/'+newfolder)
 
         new_path = os.path.join(intermediate_image_process_path,newfolder)
 
         grid = product(range(0, h - h % d, d), range(0, w - w % d, d))
-        for i, j in grid:
+        for j, k in grid:
             box = (j, i, j + d, i + d)
-            out = os.path.join(new_path, f'{name}_{i}_{j}{ext}')
+            out = os.path.join(new_path, f'{name}_{j}_{k}{ext}')
             img.crop(box).save(out)
 
         return_output.append(new_path)
 
     return return_output
 
-# read_image(unprocess_images_path)
-
-# asyncio.run(read_image(unprocess_images_path))
--- Process_Image.py
+++ Process_Image.py
@@ -1,17 +1,30 @@
 from Image_Read_Write.Read_Write_Image import read_image,unprocess_images_path
 import os
 from PIL import Image, ImageOps,ImageFilter
+import numpy as np
+import asyncio
 
-
-def blur_outline():
-   folder_path = read_image(unprocess_images_path)
+async def blur_outline():
+   folder_path = await read_image(unprocess_images_path)
 
    for impath in folder_path:
       for i in os.listdir(impath):
          img = Image.open(impath+"/"+i)
-         # img_with_border = ImageOps.expand(img, border=300, fill='black')
-         # img_with_border.save('bordered-%s' % i)
-         im1 = img.filter(ImageFilter.GaussianBlur(radius=2))
+         img_with_border = ImageOps.expand(img, border=10, fill='black')
+         img_with_border.save(impath+"/"+i)
+         im1 = img.filter(ImageFilter.GaussianBlur(radius=5))
          im1.save(impath+"/"+i)
 
-blur_outline()
+   # for impath in folder_path:
+   #
+   #   imlist = os.listdir(impath)
+   #
+   #   x = np.array([np.array(Image.open(impath+"/"+fname)) for fname in imlist])
+   #
+   #   imgFull = blockMerge(x, imgWidth = 2517, imgHeight = 3028)
+   #
+   #   # Save full image
+   #   saveBlock(imgFull)
+
+
+asyncio.run(blur_outline())
